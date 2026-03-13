a76b56af586d490cc7b885d98d72134e6377375c
fpayra-collab <fakhrirullz@gmail.com>
2026-03-13 21:52:04 +0300

    Update with all real 3D models from Google Drive

--- app.py
+++ app.py
@@ -6,22 +6,22 @@ DISHES = {
     "burger": {
         "slug": "burger",
         "name": "Signature Burger",
-        "glb": "/static/models/burger.glb",
-        "usdz": "/static/models/burger.usdz",  # optional for iPhone
+        "glb": "https://drive.google.com/uc?export=download&id=172b0p2P9wbCHkH9Blbla6h2MMfFzrHVf",
+        "usdz": "https://drive.google.com/uc?export=download&id=1i-Evet1pokp9AUuTBN6wGberm-yXcwfR",  # optional for iPhone
         "scale": "0.2 0.2 0.2",
     },
     "momo": {
         "slug": "momo",
         "name": "Momos",
-        "glb": "/static/models/momo.glb",
-        "usdz": "/static/models/momo.usdz",
+        "glb": "https://drive.google.com/uc?export=download&id=1crOsN0blLGS5lEknDzPv-SkxdMP1Gpwm",
+        "usdz": "https://drive.google.com/uc?export=download&id=1ezWmxqnx6rJbiKag-y5cZONAMBXxXJbm",
         "scale": "0.18 0.18 0.18",
     },
     "wrap": {
         "slug": "wrap",
         "name": "Wrap",
-        "glb": "/static/models/wrap.glb",
-        "usdz": "/static/models/wrap.usdz",
+        "glb": "https://drive.google.com/uc?export=download&id=1ROEfo8NvbHs1FpXeteXVYpyKWZt804nE",
+        "usdz": "https://drive.google.com/file/d/1tpTfNF9sGTFPBqM8GmJAIPNEJovpGy81/view?usp=sharing",
         "scale": "0.18 0.18 0.18",
     },
     # add more dishes...
